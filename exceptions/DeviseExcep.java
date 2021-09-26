b04c1f93feffc9fc8de94aeb22afdab0c47fb190
agathejnr4 <91242534+agathejnr4@users.noreply.github.com>
2021-09-26 16:44:54 +0200

    correction

---
+++ exceptions/DeviseExcep.java
@@ -0,0 +1,5 @@
+public class DeviseExcep extends Exception {
+    public DeviseExcep() {
+        System.out.println("La matiere n existe pas dans cette formation");
+    }
+}
---
+++ exceptions/DeviseException.java
@@ -0,0 +1,5 @@
+public class DeviseException extends Exception {
+    public DeviseException() {
+        System.out.println("La note n est pas entre 0 et 20");
+    }
+}
