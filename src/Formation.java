3d49662b3d9a2c850e957d54a48c9048f5715e28
duong <duongdk69@gmail.com>
2021-09-30 17:34:50 +0200

    Finir Formation et Test pour Formation

--- src/Formation.java
+++ src/Formation.java
@@ -1,2 +1,66 @@
+package nikel;
+import java.util.*;
 public class Formation {
+
+    private String id;
+    private HashMap<String, Integer> matieres;
+
+    public Formation (String id, HashMap<String,Integer> mat) {
+        this.id = id;
+        this.matieres = mat;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getMatieres() {
+        String res="";
+        for ( String key : this.matieres.keySet() ) {
+            res+=key;
+        }
+        return res;
+    }
+
+    @Override
+    public String toString() {
+        return "Formation [id=" + id + ", matieres=" + matieres + "]";
+    }
+
+    public void ajouterMatiere(String nomMatiere, int point) {
+        if (this.matieres.containsKey(nomMatiere)) {
+            System.out.println("Already had this one");
+        }
+        else {
+            this.matieres.put(nomMatiere, point);
+        }
+    }
+
+    public void supprimerMatiere(String nomMatiere) {
+        if (!this.matieres.containsKey(nomMatiere)) {
+            System.out.println("Not found");
+        }
+        else {
+            this.matieres.remove(nomMatiere);
+        }
+    }
+
+    public int getCoef(String nomMatiere){
+        if(!this.matieres.containsKey(nomMatiere)){
+            System.out.println("Not found Matiere");
+            return -1;
+        }
+        else{
+            return this.matieres.get(nomMatiere);
+        }
+    }
+
+    public boolean checkMatiere(String nomMatiere){
+        if(!this.matieres.containsKey(nomMatiere)){
+            System.out.println("Not found Matiere");
+            return false;
+        }
+        else { return  true;}
+    }
+
 }
---
+++ src/FormationTest.java
@@ -0,0 +1,51 @@
+import nikel.Formation;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+class FormationTest {
+
+    @Test
+    void getId() {
+        HashMap<String,Integer> mat = new HashMap<>();
+        Formation f = new Formation("123",mat);
+        assertEquals("123",f.getId());
+    }
+
+    @Test
+    void getMatieres() {
+        HashMap<String,Integer> mat = new HashMap<>();
+        Formation f = new Formation("123",mat);
+        mat.put("Math",20);
+        mat.put("Litterature",15);
+        assertEquals("LitteratureMath",f.getMatieres());
+    }
+
+    @Test
+    void testToString() {
+        HashMap<String,Integer> mat = new HashMap<>();
+        Formation f = new Formation("123",mat);
+        mat.put("Math",20);
+        mat.put("Litterature",15);
+        assertEquals("Formation [id=123, matieres={Litterature=15, Math=20}]",f.toString());
+    }
+
+    @Test
+    void ajouterMatiere() {
+        HashMap<String,Integer> mat = new HashMap<>();
+        Formation f = new Formation("123",mat);
+        f.ajouterMatiere("Math",123);
+        assertEquals(true,f.checkMatiere("Math"),"Math must been here");
+        assertEquals(123,f.getCoef("Math"),"It must be 123 coef");
+    }
+
+    @Test
+    void supprimerMatiere() {
+        HashMap<String,Integer> mat = new HashMap<>();
+        Formation f = new Formation("123",mat);
+        f.ajouterMatiere("Math",123);
+        f.supprimerMatiere("Math");
+        assertEquals(false,f.checkMatiere("Math"),"Math has been deleted");
+    }
+}
