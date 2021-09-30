6fa49a4fcc384640dd6d28d8799c1b0eaae33be2
duong <duongdk69@gmail.com>
2021-09-30 22:35:51 +0200

    Toucher que Formation Test

--- src/FormationTest.java
+++ src/FormationTest.java
@@ -1,47 +1,44 @@
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import java.util.*;
 
-class FormationTest {
+import java.util.HashMap;
 
+import static org.junit.Assert.assertEquals;
+
+public class FormationTest {
+
+    HashMap<String,Integer> mat = new HashMap<>();
+    Formation f = new Formation("123",mat);
     @Test
-    void getId() {
-        HashMap<String,Integer> mat = new HashMap<>();
-        Formation f = new Formation("123",mat);
+    public void getId() {
         assertEquals("123",f.getId());
     }
 
     @Test
-    void getMatieres() {
-        HashMap<String,Integer> mat = new HashMap<>();
-        Formation f = new Formation("123",mat);
+    public void getMatieres() {
         mat.put("Math",20);
         mat.put("Litterature",15);
-        assertEquals("LitteratureMath",f.getMatieres());
+        HashMap<String,Integer> expected = new HashMap<>();
+        expected.put("Math",20);
+        expected.put("Litterature",15);
+        assertEquals(expected,f.getMatieres());
     }
 
     @Test
-    void testToString() {
-        HashMap<String,Integer> mat = new HashMap<>();
-        Formation f = new Formation("123",mat);
+    public void testToString() {
         mat.put("Math",20);
         mat.put("Litterature",15);
         assertEquals("Formation [id=123, matieres={Litterature=15, Math=20}]",f.toString());
     }
 
     @Test
-    void ajouterMatiere() {
-        HashMap<String,Integer> mat = new HashMap<>();
-        Formation f = new Formation("123",mat);
+    public void ajouterMatiere() {
         f.ajouterMatiere("Math",123);
         assertEquals("Math must been here", true, f.checkMatiere("Math"));
         assertEquals("It must be 123 coef",123,f.getCoef("Math"));
     }
 
     @Test
-    void supprimerMatiere() {
-        HashMap<String,Integer> mat = new HashMap<>();
-        Formation f = new Formation("123",mat);
+    public void supprimerMatiere() {
         f.ajouterMatiere("Math",123);
         f.supprimerMatiere("Math");
         assertEquals("Math has been deleted", false, f.checkMatiere("Math"));
