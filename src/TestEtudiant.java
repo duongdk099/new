31d96c05153a9fdb0862ee0a6f63b322af5a75b7
agathejnr4 <91242534+agathejnr4@users.noreply.github.com>
2021-09-30 19:46:25 +0200

    Tests complets de la méthode ajouter Note (il reste à régler un problème)

--- src/TestEtudiant.java
+++ src/TestEtudiant.java
@@ -1,4 +1,73 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import static org.junit.Assert.*;
+
 class TestEtudiant {
-  
+
+    /**
+     * Creation d une identite
+     */
+    String n ="nip" ;
+    String nom = "DuPont" ;
+    String prenom = "Jean" ;
+    Identite i = new Identite(n,nom,prenom) ;
+
+    /**
+     * Creation d une formation
+     */
+    String id = "info" ;
+    HashMap<String,Integer> mat = new HashMap<String,Integer>() ;
+    Formation f = new Formation(id,mat) ;
+
+    /**
+     * creation d un etudiant
+     */
+    Etudiant e = new Etudiant(i,f) ;
+
+    /**
+     * ajout de matieres a la formation
+     */
+    f.ajouterMatiere("programmation",4) ;
+    f.ajouterMatiere("reseaux",2) ;
+
+    /**
+     * Tests
+     */
+
+    /**
+     * Tests ajouterNote
+     */
+
+    // test normal
+    @Test
+    public void ajouterNoteNormal() {
+        e.ajouterNote("programmation",15.0f) ;
+        assertTrue("15 doit etre dans la liste des notes de la matiere programmation",e.getNotes().get("programmation").contains(15.0f)) ;
+    }
+
+    // note trop elevee
+    @Test
+    public void ajouterNoteSup() {
+        e.ajouterNote("programmation",25.0f) ;
+        assertFalse("25 ne doit pas etre dans la liste des notes car la note est au dessus de 20",e.getNotes().get("programmation").contains(25.0f)) ;
+    }
+
+    // note trop basse
+    @Test
+    public void ajouterNoteInf() {
+        Float n = -5.0f ;
+        e.ajouterNote("programmation",n) ;
+        assertFalse("-5 ne doit pas etre dans la liste des notes car la note est en dessous de 0",e.getNotes().get("programmation").contains(-5.0f)) ;
+    }
+
+    @Test
+    public void calculerMoyenneMat() {
+
+    }
+
+    @Test
+    public void calculerMoyenneGe() {
+
+    }
 }
